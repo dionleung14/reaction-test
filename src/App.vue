bc1b222521d0049242acbff3ab7d28a20d48471f
NA Flipper <dioncleung@gmail.com>
2020-11-23 13:35:31 -0800

    prepared for heroku deployment. added working scare placeholder

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@
     <div v-if="started">
       time number: {{ timeNumber }}
     </div>
+    <div v-if="scare">
+      Boo
+    </div>
   </div>
 </template>
 
@@ -33,6 +36,8 @@
         timeCounter: setInterval(() => {this.timeNumber++}, 10),
         timeNumber: 0,
         started: false,
+        attempts: 0,
+        scare: false
       };
     },
     created() {
@@ -40,17 +45,27 @@
     },
     methods: {
       start() {
-        if (!this.started) {
-          this.started = !this.started;
+        if (this.attempts < 2) {
+          if (!this.started) {
+            this.started = !this.started;
+          }
+          this.attempts++;
+          let randomInt = (Math.random() * 2000) + 2000
+          console.log(randomInt / 1000)
+          setTimeout( () => {
+            this.timeCounter = setInterval(() => {this.timeNumber += 1}, 1)
+          }, randomInt)
+        } else {
+          let randomScare = (Math.random() * 2000) + 2000
+          console.log(randomScare / 1000)
+          setTimeout( () => {
+            this.scare = true
+            }, randomScare)
         }
-        let randomInt = (Math.random() * 2500) + 2000
-        console.log(randomInt / 1000)
-        setTimeout( () => {
-          this.timeCounter = setInterval(() => {this.timeNumber += 1}, 10)
-        }, randomInt)
       },
       quit() {
         clearInterval(this.timeCounter);
+        this.started = false; 
       },
       reset() {
         this.timeNumber = 0; 
